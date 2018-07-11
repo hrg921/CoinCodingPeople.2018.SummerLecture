0e426d6c703c835e087600c80fd539bb4a3c287a
hrg921 <hrg921@gmail.com>
2018-07-12 01:32:39 +0900

    page 4.3 - condition, repetition

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var isAdult = true
 
 console.log(name, age, isAdult)
 console.log(typeof name, typeof age, typeof isAdult)
-console.log(age * 3)
+console.log(age * 3, typeof (age * 3))
 console.log(age + 3)
 console.log(age - 3)
 console.log(age / 3)
@@ -20,5 +20,31 @@ if (age > 20) {
   isAdult = false
 }
 
-console.log(age)
+console.log(age > 20)
+console.log(isAdult)
 
+age = 30
+isAdult = true
+
+while(isAdult) {
+  console.log(age, isAdult)
+  age = age - 1
+  if (age > 20) {
+    isAdult = true
+  } else {
+    isAdult = false
+  }
+}
+
+var hobby1 = 'basketball'
+var hobby2 = 'singing'
+var hobby3 = 'coding'
+
+var hobbies = ['basketball', 'singing', 'coding']
+console.log(hobbies)
+console.log(hobbies[1])
+console.log(hobbies[0])
+
+for (hobby of hobbies) {
+  console.log(hobby)
+}
