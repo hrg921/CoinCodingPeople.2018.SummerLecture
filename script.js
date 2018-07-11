78b4a52243c864b377e673a829ebc6b4da635918
hrg921 <hrg921@gmail.com>
2018-07-12 01:23:31 +0900

    page 4.2 - data type, variable, operator

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log('Hello JavaScript!')
+var name = '한건호'
+var age = 21
+var isAdult = true
+
+console.log(name, age, isAdult)
+console.log(typeof name, typeof age, typeof isAdult)
+console.log(age * 3)
+console.log(age + 3)
+console.log(age - 3)
+console.log(age / 3)
+console.log(!isAdult)
+console.log(!!isAdult)
+
+age = 19
+
+if (age > 20) {
+  isAdult = true
+} else {
+  isAdult = false
+}
+
+console.log(age)
+
