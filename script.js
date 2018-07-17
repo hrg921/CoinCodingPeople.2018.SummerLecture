36962e3b043581fdf7b1b09d80b940e3424e0865
hrg921 <hrg921@gmail.com>
2018-07-18 00:08:45 +0900

    page 4.2 - data type, variable, operator

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 document.body.innerHTML = '<div><strong>hello javascript</strong></div>'
+
+var name = '한건호'
+var age = 21
+var isCouple = false
+const BIRTHDAY = 980921
+
+document.body.innerHTML += '<h1>' + name + '</h1>'
+document.body.innerHTML += '<br></br>'
+document.body.innerHTML += age
+document.body.innerHTML += '<br></br>'
+document.body.innerHTML += isCouple
+document.body.innerHTML += '<br></br>'
+document.body.innerHTML += '<strong>' + BIRTHDAY + '</strong>'
